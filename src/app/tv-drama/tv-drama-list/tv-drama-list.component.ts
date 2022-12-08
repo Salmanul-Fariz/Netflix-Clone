7c7d5cb5e90d35033b0e69a07b7251d66acbee5b
Salmanul Fariz N <salmanulfariz997@gmail.com>
2022-12-08 16:41:36 +0530

    TV drama section completed

--- src/app/tv-drama/tv-drama-list/tv-drama-list.component.ts
+++ src/app/tv-drama/tv-drama-list/tv-drama-list.component.ts
@@ -3,8 +3,19 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-tv-drama-list',
   templateUrl: './tv-drama-list.component.html',
-  styleUrls: ['./tv-drama-list.component.css']
+  styleUrls: ['./tv-drama-list.component.css'],
 })
 export class TvDramaListComponent {
-
+  images = [
+    { path: '/assets/banner1.jpg' },
+    { path: '/assets/banner1.jpg' },
+    { path: '/assets/banner1.jpg' },
+    { path: '/assets/banner1.jpg' },
+    { path: '/assets/banner1.jpg' },
+    { path: '/assets/banner1.jpg' },
+    { path: '/assets/banner1.jpg' },
+    { path: '/assets/banner1.jpg' },
+    { path: '/assets/banner1.jpg' },
+    { path: '/assets/banner1.jpg' },
+  ];
 }
