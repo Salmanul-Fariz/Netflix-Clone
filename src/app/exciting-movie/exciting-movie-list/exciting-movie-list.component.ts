730e023be3bd97e5ea2b32d33e852aa601c7add0
Salmanul Fariz N <salmanulfariz997@gmail.com>
2022-12-08 16:38:42 +0530

    exciting movie section completed

--- src/app/exciting-movie/exciting-movie-list/exciting-movie-list.component.ts
+++ src/app/exciting-movie/exciting-movie-list/exciting-movie-list.component.ts
@@ -3,8 +3,19 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-exciting-movie-list',
   templateUrl: './exciting-movie-list.component.html',
-  styleUrls: ['./exciting-movie-list.component.css']
+  styleUrls: ['./exciting-movie-list.component.css'],
 })
 export class ExcitingMovieListComponent {
-
+  images = [
+    { path: '/assets/banner1.jpg' },
+    { path: '/assets/banner1.jpg' },
+    { path: '/assets/banner1.jpg' },
+    { path: '/assets/banner1.jpg' },
+    { path: '/assets/banner1.jpg' },
+    { path: '/assets/banner1.jpg' },
+    { path: '/assets/banner1.jpg' },
+    { path: '/assets/banner1.jpg' },
+    { path: '/assets/banner1.jpg' },
+    { path: '/assets/banner1.jpg' },
+  ];
 }
